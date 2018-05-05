2dae4a053f74c94de7aa71d1ebe38f114d2bc9a7
Vanilson Santos <vanilson.m.santos@gmail.com>
2018-05-05 16:29:48 -0300

    Adds spring boot skeleton application

---
+++ src/main/java/com/booking/synopsis/bookingsynopsis/BookingSynopsisApplication.java
@@ -0,0 +1,12 @@
+package com.booking.synopsis.bookingsynopsis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSynopsisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSynopsisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/booking/synopsis/bookingsynopsis/BookingSynopsisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.booking.synopsis.bookingsynopsis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookingSynopsisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
